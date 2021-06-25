ec32552e387649473ebe7f76585e63ae0668e385
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-25 17:32:11 -0300

    configuracao api-gateway

---
+++ api-gateway/src/main/kotlin/br/com/wagner/apigateway/ApiGatewayApplication.kt
@@ -0,0 +1,15 @@
+package br.com.wagner.apigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+class ApiGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGatewayApplication>(*args)
+}
---
+++ api-gateway/src/test/kotlin/br/com/wagner/apigateway/ApiGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.wagner.apigateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
