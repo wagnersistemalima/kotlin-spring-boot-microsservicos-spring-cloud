f9f1c3dc2a5cd899bd8efd312d314406ba0bf712
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-27 14:23:51 -0300

    projeto config-server, configuracao

--- config-server/src/main/kotlin/br/com/wagner/configserver/ConfigServerApplication.kt
+++ config-server/src/main/kotlin/br/com/wagner/configserver/ConfigServerApplication.kt
@@ -2,7 +2,9 @@ package br.com.wagner.configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
 class ConfigServerApplication
 
