3e9645a7fd9b8e95e4475984d520e90f4768beb6
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-28 18:19:34 -0300

    configuracao do projeto oauth-server e cliente eureka com porta aleatoria

--- oauth-server/src/main/kotlin/br/com/wagner/oauthserver/OauthServerApplication.kt
+++ oauth-server/src/main/kotlin/br/com/wagner/oauthserver/OauthServerApplication.kt
@@ -2,7 +2,9 @@ package br.com.wagner.oauthserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class OauthServerApplication
 
